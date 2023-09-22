09ade0f8a8a9513786b23be911f76bb379e6b27b
ShradhaVastrakar <shradhavastrakar@gmail.com>
2023-09-22 22:26:51 +0530

    Swagger documentation started

---
+++ docs/swagger.js
@@ -0,0 +1,13 @@
+// ----------------------------->>>>>>> Schemas <<<<<<<-----------------------------
+
+// --------------------->>> Authorization Schema<<<---------------------
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     BearerAuth:
+ *       type: apiKey
+ *       in: header
+ *       name: Authorization
+ *       description: Bearer token authorization header
+*/
--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const { connection } = require("./configs/db");
 const app = express()
 require("dotenv").config()
 const PORT = process.env.PORT || 8080
+const colors = require("colors");
 const { userRouter } = require("./routes/userRoute");
 const { productRouter } = require("./routes/productRoute");
 const { categoryRouter } = require("./routes/categoryRoute");
 const { cartRouter } = require("./routes/cartRoute");
 const { orderRouter } = require("./routes/orderRoute");
+
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 
 require("dotenv").config();
@@ -36,6 +40,29 @@ app.use("/api/products", productRouter);
 app.use("/api/categories", categoryRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/orders", orderRouter);
+
+
+// **************************SWAGGER*****************************
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Triveous Ecommerce Backend",
+      version: "1.0.0",
+      description:
+        "This is Triveous Ecommerce API, user can register and login with valid credentials and after authentication user can see products and purchase any product with their category. If user wants to view the single product items etc. they can also view and if user wants to edit anything so they can also do this in the cart section and in order section they can either place and order and can see order history.",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080/api",
+      },
+    ],
+  },
+  apis: ["./docs/*.js"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.listen(PORT, async () => {
   try {
